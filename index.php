9184fd3a42c30d7424be7b5953212e71551fbf17
William-Aultman <119095660+William-Aultman@users.noreply.github.com>
2023-03-04 23:50:23 -0500

    Update

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <h2>What are aircraft?</h2>
     <p>The definition of aircraft is any plane, helicopter, or other machine capable of flight. This inculdes spaceships, hot air balloons, or blimp.</p>
 </div>
-<div>
+<div id="newsSection1">
     <?php
     $apikey = 'a706b43f421c58dd41bffe79e85f462b';
     $url = "https://gnews.io/api/v4/search?q=aircraft&lang=en&country=us&max=10&apikey=$apikey";
@@ -35,7 +35,27 @@
     curl_close($ch);
     $articles = $data['articles'];
 
-    for ($i = 0; $i < count($articles); $i++) {
+    for ($i = 0; $i < 5; $i++) {
+        $newsImage = $articles[$i]['image'];
+        echo " <img id='news' src=$newsImage width='100px' height='100px'/>";
+        echo nl2br ("\n");
+        echo nl2br ("\n");
+        // articles[i].title
+        echo nl2br ("Title: " . $articles[$i]['title'] . "\n");
+        // articles[i].description
+        echo nl2br ("Description: " . $articles[$i]['description'] . "\n");
+        echo " <a href=".$articles[$i]['url'].">Source</a>";
+        echo nl2br ("\n");
+        // You can replace {property} below with any of the article properties returned by the API.
+        // articles[i].{property}
+        // echo $articles[$i]['{property}'] . "\n";
+        echo nl2br ("\n");
+    }
+    ?>
+</div>
+<div id="newsSection2">
+    <?php
+    for ($i = 5; $i < 10; $i++) {
         $newsImage = $articles[$i]['image'];
         echo " <img id='news' src=$newsImage width='100px' height='100px'/>";
         echo nl2br ("\n");
